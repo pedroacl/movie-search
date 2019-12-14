482ad77168560688417aded3e6114cbbcaebbcad
Pedro Luis <pedroalexandrecl@gmail.com>
2019-12-14 10:18:21 +0000

    Add main project structure

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from 'containers/Home'
+import MovieDetails from 'containers/MovieDetails'
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/details">
+          <MovieDetails />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
