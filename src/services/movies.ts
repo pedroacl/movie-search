f4f3aad0555284bfc33c31d7e5b5bb08d3522c7e
Pedro Luis <pedroalexandrecl@gmail.com>
2019-12-15 00:00:25 +0000

    Minor changes

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -2,14 +2,13 @@ import axios from 'axios'
 
 const imdbAPI = 'http://www.omdbapi.com'
 const apiKey = '2c5cb6b8'
-// const imdbAPI = 'www.omdbapi.com/?i=tt3896198&apiKey=2c5cb6b8'
 
 export const getMovies = (search: string) => {
-  const params = {s: search, apikey: apiKey}
-  return axios.get(imdbAPI, {params})
+  const params = { s: search, apikey: apiKey }
+  return axios.get(imdbAPI, { params })
 }
 
 export const getMovie = (imdbID: string) => {
-  const params = {i: imdbID, apikey: apiKey}
-  return axios.get(imdbAPI, {params})
+  const params = { i: imdbID, apikey: apiKey }
+  return axios.get(imdbAPI, { params })
 }
